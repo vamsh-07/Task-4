a7e170c3155d4013a5880681403e8e1ec0b5835f
vamsh-07 <vamsivamsh192@gmail.com>
2025-05-22 23:11:07 +0530

    Create mainactivity.kt

---
+++ mainactivity.kt
@@ -0,0 +1,10 @@
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+    }
+}
